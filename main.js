d738556916a6e50b5d57b06cedc3cc4f8f4af179
Papuna Maglaperidze <papunamaglaperidze@Papunas-MacBook-Pro.local>
2023-02-18 21:28:59 -0800

    hovering was added to css file

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const questions = document.getElementsByClassName('question');
-const arrow = document.getElementsByClassName('arrow');
+
+// This function drops down the paragraph
 
 for (var i = 0; i < questions.length; i++) {
     questions[i].addEventListener('click', function(){
         this.classList.toggle('active');
+       
         var x = this.nextElementSibling;
         if (x.style.display === 'block') {
-            x.style.display = 'none';
+            x.style.display = 'none'
         }   else {
-            x.style.display = 'block';
+            x.style.display = 'block'
         }
-
     });
+
 }
-
-
